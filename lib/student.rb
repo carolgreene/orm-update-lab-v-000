3a86d76650976215e3c6791ad578833aa13783f9
Carol Greene <carolgreene3@gmail.com>
2018-03-11 18:52:07 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -50,9 +50,9 @@ class Student
 
   def self.new_from_db(row)
     student = Student.new(row[0], row[1], row[2])
-    new_student.id = row[0]
-    new_student.name = row[1]
-    new_student.grade = row[2]
+    #new_student.id = row[0]
+    #new_student.name = row[1]
+    #new_student.grade = row[2]
     student
   end
 
